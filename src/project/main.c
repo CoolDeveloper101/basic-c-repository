27caea495f828c748e25d9d1e61342b5a2c1e09c
CoolDeveloper101 <cooldeveloper101@gmail.com>
2020-03-20 12:16:48 +0530

    Adding the factorial function

--- src/project/main.c
+++ src/project/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int factorial(int n);
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n", factorial(10));
     return 0;
+}
+
+int factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
